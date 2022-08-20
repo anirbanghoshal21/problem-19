eda90157d3cf22d2c0c145fe0c429600a4373a35
username <username@cognizant.com>
2022-08-20 23:35:58 +0530

    Add project files.

---
+++ problem 19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c1, c2;
+
+            Console.WriteLine("Enter the first character : ");
+            c1 = Console.ReadLine()[0];
+
+            Console.WriteLine("Enter the second character : ");
+            c2 = Console.ReadLine()[0];
+
+            if (c1 == c2)
+                Console.WriteLine("Both characters are equal");
+            else
+                Console.WriteLine("Characters are not equal");
+            Console.ReadLine();
+        }
+    }
+}
